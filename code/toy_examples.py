e736b50c680fcf7a1c77585323fd12c78d576a37
Jose Velarde <joseivm@mit.edu>
2022-08-17 10:18:05 -0500

    adding single zone eval and cleaning up code

--- code/toy_examples.py
+++ code/toy_examples.py
@@ -16,12 +16,14 @@ TABLES_DIR = PROJECT_DIR + '/output/tables'
 # TODO: 1. figure out how to include positive and negative losses as well
 # 2. figure out how to set w_bar when w is supposed to be loss
 
+##### Data Creation #####
 def make_single_zone_data(beta,mu,sigma,n=100):
     theta = np.random.normal(loc=mu,scale=sigma,size=n)
     epsilon = np.random.normal(loc=0,scale=1)
     w = beta*theta + epsilon
     return w, theta.reshape(-1,1)
 
+##### Optimization Functions #####
 def min_premium_program(pred_y,train_y,loss_p,epsilon,model='full',a_lb=0):
     w_bar = np.quantile(train_y,loss_p)
     n_samples = train_y.shape[0]
@@ -55,39 +57,59 @@ def min_premium_program(pred_y,train_y,loss_p,epsilon,model='full',a_lb=0):
     return_dict = {'full':(a.value,b.value),'slope':(a.value,0),'intercept':(1,b.value)}
     return return_dict[model]
 
-def min_CVaR_program(pred_y,train_y,max_pi,epsilon,model='full',a_lb=0):
+def min_CVaR_program(pred_y,train_y,params):
+    max_pi = params['max_premium']
+    K = params['K']
+    epsilon = params['epsilon']
+
     n_samples = train_y.shape[0]
-    I = cp.Variable(n_samples)
     a = cp.Variable()
     b = cp.Variable()
     t = cp.Variable()
-    # max_payout = cp.Variable()
-    max_payout = 30
     gamma = cp.Variable(n_samples)
 
     constraints = []
-    constraints.append(I >= 0)
-    constraints.append(I <= max_payout)
-    constraints.append((1/n_samples)*sum(I) <= max_pi*max_payout)
-    constraints.append(gamma >=  train_y + (1/n_samples)*sum(I) - I -t)
+    constraints.append(a*pred_y+b >= 0)
+    constraints.append(a*pred_y+b >= gamma + train_y -t)
+    constraints.append(K >= gamma + train_y -t)
     constraints.append(gamma >= 0)
-
-    if model == 'full':
-        constraints.append(I >= a*pred_y - b)
-        constraints.append(b >= 0)
-        constraints.append(a >= a_lb)
-    elif model == 'slope':
-        constraints.append(I >= a*pred_y)
-        constraints.append(a >= a_lb)
-    else:
-        constraints.append(I >= pred_y - b)
-        constraints.append(b >= 0)
+    constraints.append(a*(1/n_samples)*sum(pred_y) + b <= K*max_pi)
 
     objective = cp.Minimize(t + (1/epsilon)*(1/n_samples)*(sum(gamma)))
     problem = cp.Problem(objective,constraints)
     problem.solve(solver=cp.GLPK)
-    return_dict = {'full':(a.value,b.value,I),'slope':(a.value,0,I),'intercept':(1,b.value,I)}
-    return return_dict[model] 
+    return (a.value, b.value)
+
+##### Evaluation functions #####
+def plot_payout_functions(ax,bdf,odf,a,b,args):
+    ax.plot(bdf['PredictedLosses'],bdf['Premium'],'bs',label='baseline')
+    ax.plot(odf['PredictedLosses'],odf['Premium'],'g^',label='opt')
+    ax.set_title(str(args))
+    ax.text(3,15,'a = {}, b = {}'.format(a,b))
+    ax.legend()
+
+def make_eval_dfs(test_x,test_y,pred_model,strike_val,a,b,K):
+    loss_quantiles = np.quantile(test_y,[0.7,0.8,0.9,0.95])
+    col_names = ['$P(L > {})$'.format(i) for i in [70,80,90,95]]
+    
+    bdf = pd.DataFrame()
+    odf = pd.DataFrame()
+    bdf['Losses'] = test_y
+    bdf['PredictedLosses'] = pred_model.predict(test_x)
+    bdf['Payout'] = np.maximum(bdf['PredictedLosses']-strike_val,0)
+    bdf['NetLoss'] = bdf['Losses'] - bdf['Payout']
+    
+    odf['Losses'] = test_y
+    odf['PredictedLosses'] = pred_model.predict(test_x)
+    odf['Payout'] = np.minimum(a*odf['PredictedLosses']+b,K)
+    odf['NetLoss'] = odf['Losses']-odf['Payout']
+    
+    for col_name, loss_quantile in zip(col_names,loss_quantiles):
+        bdf[col_name] = bdf['NetLoss'] > loss_quantile
+        odf[col_name] = odf['NetLoss'] > loss_quantile
+    odf = odf.rename(columns={'Payout':'Premium'})
+    bdf = bdf.rename(columns={'Payout':'Premium'})
+    return bdf, odf
 
 def determine_strike_value(train_y,eval_y,eval_x,pred_model):
     strike_percentiles = np.arange(0.1,0.35,0.05)
@@ -107,35 +129,43 @@ def determine_strike_value(train_y,eval_y,eval_x,pred_model):
     best_strike_percentile, best_strike_val = max(strike_performance,key=strike_performance.get)
     return best_strike_percentile, best_strike_val
 
-def make_eval_dfs(test_x,test_y,pred_model,strike_val,a,b):
-    loss_quantiles = np.quantile(test_y,[0.7,0.8,0.9,0.95])
-    col_names = ['P(L > {})'.format(i) for i in [70,80,90,95]]
-    
-    bdf = pd.DataFrame()
-    odf = pd.DataFrame()
-    bdf['Losses'] = test_y
-    bdf['PredictedLosses'] = pred_model.predict(test_x)
-    bdf['Payout'] = np.maximum(bdf['PredictedLosses']-strike_val,0)
-    bdf['NetLoss'] = bdf['Losses'] - bdf['Payout']
-    
-    odf['Losses'] = test_y
-    odf['PredictedLosses'] = pred_model.predict(test_x)
-    odf['Payout'] = np.maximum(a*odf['PredictedLosses']+b,0)
-    odf['NetLoss'] = odf['Losses']-odf['Payout']
-    
-    for col_name, loss_quantile in zip(col_names,loss_quantiles):
-        bdf[col_name] = bdf['NetLoss'] > loss_quantile
-        odf[col_name] = odf['NetLoss'] > loss_quantile
-    odf = odf.rename(columns={'Payout':'Premium'})
-    bdf = bdf.rename(columns={'Payout':'Premium'})
-    return bdf, odf
+##### Exploration functions #####
 
-def plot_payout_functions2(ax,bdf,odf,a,b,args):
-    ax.plot(bdf['PredictedLosses'],bdf['Premium'],'bs',label='baseline')
-    ax.plot(odf['PredictedLosses'],odf['Premium'],'g^',label='opt')
-    ax.set_title('{}: {}, {}: {}'.format(*args))
-    ax.text(3,15,'a = {}, b = {}'.format(a,b))
-    ax.legend()
+def parameter_exploration(data,params,model):
+    model_predictions = data['model predictions']
+    train_y = data['train_y']
+    test_x = data['test_x']
+    test_y = data['test_y']
+    strike_val = data['strike val']
+    loop_param = [k for k,v in params.items() if type(v) is np.ndarray][0]
+    loop_param_vals = params[loop_param]
+    param_subdict = {k: v for k,v in params.items() if type(v) is not np.ndarray}
+    fig, axes = plt.subplots(5,1,figsize=(10,10))
+    ldf = pd.DataFrame()
+    for loop_param_val, ax in zip(loop_param_vals, axes.ravel()):
+        loop_params = dict(list(param_subdict.items()) + [(loop_param,loop_param_val)])
+        a,b = min_CVaR_program(model_predictions,train_y,loop_params)
+        a,b = np.around((a,b),2)
+        bdf, odf = make_eval_dfs(test_x,test_y,pred_model,strike_val,a,b,loop_params['K'])
+        plot_payout_functions(ax,bdf,odf,a,b,loop_params)
+        
+        opt_stats = odf.drop(columns=['Losses','PredictedLosses','NetLoss']).mean().to_dict()
+        opt_stats[loop_param] = str(loop_param_val)
+        ldf = ldf.append(opt_stats,ignore_index=True)
+        
+        base_stats = bdf.drop(columns=['Losses','PredictedLosses','NetLoss']).mean().to_dict()
+        base_stats[loop_param] = 'Baseline'
+        ldf = ldf.append(base_stats,ignore_index=True)
+                         
+    fig_dir = FIGURES_DIR + '/' + model
+    table_dir = TABLES_DIR + '/' + model
+
+    plt.tight_layout()
+    filename = fig_dir+'/{}_exploration.png'.format(loop_param)
+    plt.savefig(filename)
+    
+    table_filename = table_dir +'/{}_exploration.tex'.format(loop_param)
+    ldf.drop_duplicates().sort_values(loop_param).to_latex(table_filename,float_format='%.2f')
 
 def premium_program_exploration(model):
     train_y, train_x = make_single_zone_data(beta=3,mu=5,sigma=2,n=100)
@@ -201,7 +231,7 @@ def premium_program_exploration(model):
     table_filename = table_dir +'/epsilon_exploration_{}.tex'.format(model)
     ldf.drop_duplicates().sort_values('Eps').to_latex(table_filename,float_format='%.2f')
 
-def CVaR_program_exploration(model):
+def CVaR_program_exploration(model_name='CVaR'):
     train_y, train_x = make_single_zone_data(beta=3,mu=5,sigma=2,n=100)
     eval_y, eval_x = make_single_zone_data(beta=3,mu=5,sigma=2,n=20)
     test_y, test_x = make_single_zone_data(beta=3,mu=5,sigma=2,n=1000)
@@ -209,62 +239,27 @@ def CVaR_program_exploration(model):
     pred_model = LinearRegression().fit(train_x,train_y)
     model_predictions = pred_model.predict(train_x)
     strike_p, strike_val = determine_strike_value(train_y,eval_y,eval_x,pred_model)
-    
-    fig_dir = FIGURES_DIR + '/CVaR2'
-    table_dir = TABLES_DIR + '/CVaR2'
+
+    data = {'train_y':train_y,'test_x':test_x,'test_y':test_y,
+            'model predictions':model_predictions,'strike val':strike_val}
     
 #     premium exploration
     epsilon = 0.2
+    K = 20
     premiums = np.around(np.linspace(0.1,0.7,5),3)
-    fig, axes = plt.subplots(5,1,figsize=(10,10))
-    ldf = pd.DataFrame()
-    for premium, ax in zip(premiums,axes.ravel()):
-        a,b,I = min_CVaR_program(model_predictions,train_y,premium,epsilon,model)
-        a,b = np.around((a,b),2)
-        bdf, odf = make_eval_dfs(test_x,test_y,pred_model,strike_val,a,b)
-        plot_payout_functions2(ax,bdf,odf,a,b,('epsilon',epsilon,'premium',premium))
-        
-        opt_stats = odf.drop(columns=['Losses','PredictedLosses','NetLoss']).mean().to_dict()
-        opt_stats['Max Premium'] = str(premium)
-        ldf = ldf.append(opt_stats,ignore_index=True)
-        
-        base_stats = bdf.drop(columns=['Losses','PredictedLosses','NetLoss']).mean().to_dict()
-        base_stats['Max Premium'] = 'Baseline'
-        ldf = ldf.append(base_stats,ignore_index=True)
+    parameter_exploration(data,{'epsilon':epsilon,'K':K,'max_premium':premiums},model_name)
                          
-    plt.tight_layout()
-    filename = fig_dir+'/premium_exploration_{}.png'.format(model)
-    plt.savefig(filename)
-    
-    table_filename = table_dir +'/premium_exploration_{}.tex'.format(model)
-    ldf.drop_duplicates().sort_values('Max Premium').to_latex(table_filename,float_format='%.2f')
-                         
-#     epsilon exploration
-    epsilons = np.around(np.linspace(0.1,0.5,5),3)
-    premium = 0.5
-    fig, axes = plt.subplots(5,1,figsize=(10,10))
-    ldf = pd.DataFrame()
-    for eps,ax in zip(epsilons,axes.ravel()):
-        # print('eps: {}, premium: {}'.format(eps,premium))
-        a,b,I = min_CVaR_program(model_predictions,train_y,premium,eps,model)
-        a,b = np.around((a,b),2)
-        bdf, odf = make_eval_dfs(test_x,test_y,pred_model,strike_val,a,b)
-        plot_payout_functions2(ax,bdf,odf,a,b,('epsilon',eps,'premium',premium))
-        
-        opt_stats = odf.drop(columns=['Losses','PredictedLosses','NetLoss']).mean().to_dict()
-        opt_stats['Eps'] = str(eps)
-        ldf = ldf.append(opt_stats,ignore_index=True)
-        
-        base_stats = bdf.drop(columns=['Losses','PredictedLosses','NetLoss']).mean().to_dict()
-        base_stats['Eps'] = 'Baseline'
-        ldf = ldf.append(base_stats,ignore_index=True)
-                         
-    plt.tight_layout()
-    filename = fig_dir+'/epsilon_exploration_{}.png'.format(model)
-    plt.savefig(filename)
-    
-    table_filename = table_dir +'/epsilon_exploration_{}.tex'.format(model)
-    ldf.drop_duplicates().sort_values('Eps').to_latex(table_filename,float_format='%.2f')
+#     K exploration
+    Ks = np.around(np.linspace(20,40,5),3)
+    epsilon = 0.2
+    premium = 0.2
+    parameter_exploration(data,{'epsilon':epsilon,'K':Ks,'max_premium':premium},model_name)
+
+# Epsilon Exploration
+    K = 30
+    epsilons = np.around(np.linspace(0.1,0.8,5),3)
+    premium = 0.3
+    parameter_exploration(data,{'epsilon':epsilons,'K':K,'max_premium':premium},model_name)
 
 np.random.seed(1)
 train_y, train_x = make_single_zone_data(beta=3,mu=5,sigma=2,n=100)
@@ -275,21 +270,22 @@ pred_model = LinearRegression().fit(train_x,train_y)
 pred_y = pred_model.predict(train_x)
 strike_p, strike_val = determine_strike_value(train_y,eval_y,eval_x,pred_model)
 
-epsilon = 0.2
-max_pi = 0.25
+epsilon = 0.1
+max_pi = 0.9
+K = 15
 plt.close()
-a,b,I = min_CVaR_program(pred_y,train_y,max_pi,epsilon)
+a,b = np.around(min_CVaR_program(pred_y,train_y,max_pi,epsilon,K),2)
 
 edf = pd.DataFrame()
-edf['OptPayout'] = I.value
 edf['PredictedLoss'] = pred_y
+edf['OptPayout'] = np.minimum(a*edf['PredictedLoss']-b,K)
 edf['BasePayout'] = np.maximum(edf['PredictedLoss']-strike_val,0)
 
 plt.plot(edf['PredictedLoss'],edf['BasePayout'],'bs',label='baseline')
 plt.plot(edf['PredictedLoss'],edf['OptPayout'],'g^',label='opt')
 plt.legend()
 plt.title('epsilon: {}, pi: {}'.format(epsilon,max_pi))
-# plt.text(3,10,'a = {}, b = {}'.format(a,b))
+plt.text(3,10,'a = {}, b = {}'.format(a,b))
 plt.xlabel(r'$\hat{l}(\theta^k)$')
 plt.ylabel('Payout, I')
 plt.show()
